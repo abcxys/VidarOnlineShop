f64600e3e0115f89ef2ee667d1b8ae238b3afe7d
abcxys <abcxys92@gmail.com>
2024-02-15 23:57:05 -0500

    Add datatable views pojos and service/implements.

--- src/main/java/com/gmail/merikbest2015/ecommerce/controller/PackingController.java
+++ src/main/java/com/gmail/merikbest2015/ecommerce/controller/PackingController.java
@@ -1,14 +1,20 @@
 package com.gmail.merikbest2015.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.gmail.merikbest2015.ecommerce.constants.Pages;
 import com.gmail.merikbest2015.ecommerce.constants.PathConstants;
+import com.gmail.merikbest2015.ecommerce.domain.DatatablesView;
+import com.gmail.merikbest2015.ecommerce.service.PackingService;
+import com.gmail.merikbest2015.ecommerce.domain.Order;
 
 import lombok.RequiredArgsConstructor;
+import net.sf.json.JSONObject;
 
 /**
  * @author yishi.xing
@@ -19,9 +25,19 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(PathConstants.PACKING)
 public class PackingController {
 	
+	@Autowired
+	private PackingService packingService;
+	
 	@GetMapping
     public String getPacking(Model model) {
         //model.addAttribute("perfumes", cartService.getPerfumesInCart());
         return Pages.PACKING;
     }
+	
+	@RequestMapping(value = "/showOrders", method = RequestMethod.POST)
+	public String getAllOrders() {
+		DatatablesView<Order> datatablesView = packingService.getAllOrders();
+		
+		return JSONObject.fromObject(datatablesView).toString();
+	}
 }
