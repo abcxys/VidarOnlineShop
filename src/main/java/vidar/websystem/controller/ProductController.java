8f16122de7bac0b9c0237f315df345620795fc8e
abcxys <abcxys92@gmail.com>
2024-03-22 16:59:48 -0400

    Add checkProductStock api in product controller TODO: add current time information as the input argument.

--- src/main/java/vidar/websystem/controller/ProductController.java
+++ src/main/java/vidar/websystem/controller/ProductController.java
@@ -9,11 +9,16 @@ import vidar.websystem.service.ProductService;
 import vidar.websystem.utils.ControllerUtils;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,5 +46,11 @@ public class ProductController {
     public String searchPerfumes(SearchRequest request, Model model, Pageable pageable) {
         controllerUtils.addPagination(request, model, perfumeService.searchProducts(request, pageable));
         return Pages.PRODUCTS;
+    }
+    
+    @RequestMapping(value="/checkProductStock", method = RequestMethod.POST)
+    @ResponseBody
+    public String checkProductStock(@RequestParam("productId") Long productId, @RequestParam("quantity") Long quantity) {
+    	return "Success";
     }
 }
