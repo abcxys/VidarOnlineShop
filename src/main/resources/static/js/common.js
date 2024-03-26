310ca41c462c306d31013fa698c93ba48c5ad2c1
abcxys <abcxys92@gmail.com>
2024-03-26 16:47:20 -0400

    Add Jquery-bootbox support, to show alert box properly.

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -9,3 +9,17 @@ function emptyValueSelector(inputId){
 	document.getElementById(inputId).options.selectedIndex = 0; //回到初始状态
 	$("#" + inputId).selectpicker('refresh');
 }
+
+function bootboxAlertError(message) {
+	var dialog = bootbox.dialog({
+		title: '<i class="fa fa-fw fa-times-circle" style="color: #dd4b39;"></i>&nbsp;' + 'Error' +'',
+		message: "<p>" + message + "</p>",
+		buttons: {
+			ok: {
+				label: 'Close',
+				className: 'btn-danger',
+				callback: function() {}
+			}
+		}
+	});
+}
