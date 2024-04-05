be56390ebaeb21f6730a5d30f219d50857d33dd1
abcxys <abcxys92@gmail.com>
2024-04-05 11:34:02 -0400

    Update the dev-testing sql queries.
    Just to make sure every hardwoodfloor entry has the batch attribute.

--- src/main/resources/db/migration/V3__Add_products.sql
+++ src/main/resources/db/migration/V3__Add_products.sql
@@ -7,12 +7,12 @@ VALUES (3, 'Germany', null, '6OLMK533.jpg', 3.79, 2000, 3, 5, 1, 1, 1, '7O-DFW44
 INSERT INTO hardwoodfloors (id, country, description, filename, price, year, plank_size_id, plank_color_id, wood_species_id, plank_type_id, grade_id, batch_id, cartons_per_skid, wear_layer_thickness) 
 VALUES (4, 'Germany', null, '6OSMG443.jpg', 3.59, 2001, 4, 6, 1, 1, 1, '8O-SMG443', 45, 2);
 INSERT INTO hardwoodfloors (id, country, description, filename, price, year, plank_size_id, plank_color_id, wood_species_id, plank_type_id, grade_id, batch_id, cartons_per_skid, wear_layer_thickness) 
-VALUES (5, 'Germany', null, '6OMRN527.png', 2.69, 2016, 1, 1, 2, 1, 1, null, null, 1.6);
+VALUES (5, 'Germany', null, '6OMRN527.png', 2.69, 2016, 1, 1, 2, 1, 1, '6O-MRN527', null, 1.6);
 INSERT INTO hardwoodfloors (id, country, description, filename, price, year, plank_size_id, plank_color_id, wood_species_id, plank_type_id, grade_id, batch_id, cartons_per_skid, wear_layer_thickness) 
-VALUES (6, 'Great Britain', null, '6ODAS318.png', 3.49, 2006, 1, 2, 1, 1, 1, null, 48, 1.6);
+VALUES (6, 'Great Britain', null, '6ODAS318.png', 3.49, 2006, 1, 2, 1, 1, 1, '6O-DAS318', 48, 1.6);
 INSERT INTO hardwoodfloors (id, country, description, filename, price, year, plank_size_id, plank_color_id, wood_species_id, plank_type_id, grade_id, batch_id, cartons_per_skid, wear_layer_thickness) 
-VALUES (7, 'Great Britain', null, '6OSST537.png', 3.29, 2016, 1, 2, 1, 1, 1, null, 48, 1.6);
+VALUES (7, 'Great Britain', null, '6OSST537.png', 3.29, 2016, 1, 2, 1, 1, 1, '6O-SST337', 48, 1.6);
 INSERT INTO hardwoodfloors (id, country, description, filename, price, year, plank_size_id, plank_color_id, wood_species_id, plank_type_id, grade_id, batch_id, cartons_per_skid, wear_layer_thickness) 
-VALUES (8, 'Great Britain', null, '6OTOC676.png', 3.69, 2008, 1, 1, 1, 1, 1, null, 48, 2);
-INSERT INTO hardwoodfloors (id, country, description, filename, price, year, plank_size_id, plank_color_id, wood_species_id, plank_type_id, grade_id, batch_id, cartons_per_skid) 
-VALUES (9, 'China', null, '7HSSH121.png', 3.59, 2008, 2, 7, 3, 1, 1, null, 45);
+VALUES (8, 'Great Britain', null, '6OTOC676.png', 3.69, 2008, 1, 1, 1, 1, 1, '6O-TOC676', 48, 2);
+INSERT INTO hardwoodfloors (id, country, description, filename, price, year, plank_size_id, plank_color_id, wood_species_id, plank_type_id, grade_id, batch_id, cartons_per_skid, wear_layer_thickness) 
+VALUES (9, 'China', null, '7HSSH121.png', 3.59, 2008, 2, 7, 3, 1, 1, '7H-SSH121', 45, 2);
