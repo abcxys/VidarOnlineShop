05d4db80656d73e6d5566867934a9c203ff90eed
abcxys <abcxys92@gmail.com>
2024-04-08 13:01:59 -0400

    Update the frontend InventoryItem table behavior.
    Each cell of the table has to be editable.

--- src/main/resources/static/js/update-inventory.js
+++ src/main/resources/static/js/update-inventory.js
@@ -1,4 +1,5 @@
 var tables;
+var subtable;
 $(function() {
 	tables = $('#updateInventoryTable').DataTable({
 	"serverSide" : true,//分页，取数据等等的都放到服务端去
@@ -56,17 +57,142 @@ $(function() {
 		targets: -1
 	}]
 });
+subtable = $('#updateInventoryItemTable').DataTable({
+	"serverSide" : true,//分页，取数据等等的都放到服务端去
+	"lengthChange": true,
+	"order": [],
+	"bProcessing" : true,
+	"autoWidth": false,
+	"searching" : false,
+	"bInfo": false,
+	"paging": false,
+	ajax : {
+		type : "post",
+		url : "/inventory/getInventoryByProductId",
+		dataSrc : "data",
+		data : function () {
+			return {
+				productId: $('#editInventoryProductId').val()
+			};
+		},
+		"error": function (data) {
+			alert("error");
+		}
+	},
+	columns : [
+		{"data" : 'id', "bSortable" : false},
+		{"data" : 'productId', "bSortable" : true},
+		{"data" : 'location', "bSortable" : true},
+		{"data" : 'quantity', "bSortable" : true},
+		{"data" : '', "bSortable" : false}
+	],
+	'columnDefs': [{
+		'targets': 0,
+		'visible': false
+	},{
+		'targets': 1,
+		'visible': false
+	},{
+		targets: -1,
+		data: null,
+		defaultContent: '<i class="fas fa-light fa-edit editInventory" style="cursor: pointer;"></i>'
+	},{
+		targets: '_all',
+		createdCell: createdCell
+	}]
+});
 tables.on('click', '.editInventory', function (e) {
 	var rowData = tables.row($(this).closest('tr')).data();
-	console.log(rowData.id);
-    $("#editInventoryModal").modal('show');
+	$('#editInventoryProductId').val(rowData.id);
+	$("#editInventoryModal").modal('show');
+	subtable.columns.adjust().draw();
 });
 
 $("#searchProductInventoryBtn").on("click", function () {
 	tables.draw();
 });
+
+$("#addNewInventoryBtn").on("click", function() {
+	console.log("Add!");
+	var mockInventory = {
+		id: "",
+		location: "",
+		quantity: ""
+	}
+	var fragmentHtml = '<th:block th:replace="fragments/inventory-fragments :: dumb-inventory-item()" />';
+	var testDiv = '<form class="form-horizontal" method="post">' +
+		'<div class="modal-body">'+
+		'<div class="form-group row">' +
+		'<div style="display: none;"><span>' + mockInventory.id + '</span></div>' +
+		'<div class="col-md-2">' +
+		'<label class="control-label xrequired">Location: </label>' +
+		'<input type="text" value="' + mockInventory.location + '" style="height: 50%;">' +
+		'</div>' +
+		'<div class="col-md-2 ml-auto">' +
+		'<label class="control-label xrequired">Quantity: </label>' +
+		'<input type="number" value="' + mockInventory.quantity + '" style="height: 50%;">' +
+		'</div>' +
+		'<div class="col-md-2 ml-auto">' +
+		'<button type="submit" class="btn btn-primary" style="position: absolute;bottom: 0;">' +
+		'<i class="fas fa-pencil"></i>' +
+		'</button>' +
+		'</div>' +
+		'</div>' +
+		'</div>' +
+		'</form>';
+	$('<div>').html(testDiv).insertBefore('.modal-footer');
+});
 });
 
 function closeEditInventoryModal() {
 	$("#editInventoryModal").modal('hide');
+}
+
+const createdCell = function(cell) {
+	let original;
+	cell.setAttribute('contenteditable', true)
+	cell.setAttribute('spellcheck', false)
+	cell.addEventListener("focus", function(e) {
+		original = e.target.textContent
+	})
+	cell.addEventListener("blur", function(e) {
+		if (original !== e.target.textContent) {
+			const row = subtable.row(e.target.parentElement)
+			row.invalidate()
+			console.log('Row changed: ', row.data())
+		}
+	})
+}
+
+function editInventoryFun() {
+	var inventoryId = $("input[name=inventoryId]").val();
+	var location = $("input[name=location]").val();
+	var quantity = $("input[name=quantity]").val();
+	if (location == '') {
+		bootboxAlertError("Location can not be empty");
+		return false;
+	}
+	if (quantity == '') {
+		bootboxAlertError("Quantity can not be empty");
+		return false;
+	}
+	/*
+	$.ajax({
+		type:"POST",
+		url:'/product/addNewColour',
+		data:{
+			colourName: colourName,
+			colourAlias: colourAlias,
+			description: colourDescription
+		},
+		success:function(response){
+			bootboxAlertPrompt(response);
+			setTimeout(function(){location.reload();},2000);
+		},
+		error:function (xhr){
+			bootboxAlertError(xhr.responseText);
+			setTimeout(function(){location.reload();}, 3000);
+		}
+	});
+	*/
 }
