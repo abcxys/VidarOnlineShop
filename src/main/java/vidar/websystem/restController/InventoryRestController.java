2040ee6e3e4c89622fd7ff02d84815739855b446
abcxys <abcxys92@gmail.com>
2024-04-10 09:44:58 -0400

    Add 'updateInventoryItem' for PutMapping for url '/inventory/update'.
    With input parameter validation and actual update validation.

--- src/main/java/vidar/websystem/restController/InventoryRestController.java
+++ src/main/java/vidar/websystem/restController/InventoryRestController.java
@@ -1,28 +1,32 @@
 package vidar.websystem.restController;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import formbean.InventoryFilterConditionForm;
 import lombok.RequiredArgsConstructor;
 import net.sf.json.JSONObject;
 import vidar.websystem.constants.PathConstants;
-import vidar.websystem.domain.DatatablesView;
-import vidar.websystem.domain.InventoryItem;
-import vidar.websystem.domain.ProductInventoryItem;
+import vidar.websystem.domain.*;
 import vidar.websystem.service.InventoryService;
+import vidar.websystem.service.UserService;
+
+import java.math.BigDecimal;
 
 /**
  * @author yishi.xing
  * create datetime Apr 4, 2024 - 4:19:14 PM
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(PathConstants.INVENTORY)
 public class InventoryRestController {
-	@Autowired
-	private InventoryService inventoryService;
-	
+	private final InventoryService inventoryService;
+	private final UserService userService;
+
 	@RequestMapping(value = "/getFilteredProductStocks", method = RequestMethod.POST)
 	public String getAllInventoryItems(InventoryFilterConditionForm inventoryFilterConditionForm) {
 		//DatatablesView<ProductInventoryItem> datatablesView = inventoryService.getAllInventoryItems();
@@ -40,7 +44,31 @@ public class InventoryRestController {
 
 	@PostMapping(value = "/getInventoryByProductId")
 	public String getInventoryByProductId(@RequestParam("productId") Integer productId) {
+		if (productId == null)
+			return "Error";
 		DatatablesView<InventoryItem> datatablesView = inventoryService.getInventoryItemsByProductId(productId);
 		return JSONObject.fromObject(datatablesView).toString();
+	}
+
+	@PutMapping(value = "/update")
+	public ResponseEntity<?> updateInventoryItem(@RequestParam("id") Long id,
+												 @RequestParam("location") String location,
+												 @RequestParam("quantity") BigDecimal quantity){
+		log.info("updating inventory id = " + String.valueOf(id));
+		log.info("updating inventory location = " + location);
+		log.info("updating inventory quantity = " + String.valueOf(quantity));
+		if (!inventoryService.existsLocationTorontoWarehouse(location))
+			return ResponseEntity.badRequest().body("Location does not exist!");
+		//TODO: do we need to validate the value of quantity?
+		Inventory inventory = inventoryService.getInventoryById(id);
+		Long locationId = inventoryService.getLocationIdByBayAndWarehouseId(location,1L);
+		// if content of inventory item does not change at all.
+		if (locationId.equals(inventory.getLocationId()) && quantity.compareTo(inventory.getCurrentQuantity()) == 0)
+			return ResponseEntity.badRequest().body("No update to the inventory!");
+		// still only working on Toronto warehouse
+		inventory.setLocationId(locationId);
+		inventory.setCurrentQuantity(quantity);
+		User user = userService.getAuthenticatedUser();
+		return ResponseEntity.ok(inventoryService.updateInventory(user, inventory));
 	}
 }
