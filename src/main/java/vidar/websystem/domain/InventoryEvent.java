3ea058b0f4e1b5db233139681ff501e2da9fa2cb
abcxys <abcxys92@gmail.com>
2024-04-10 10:36:29 -0400

    Add entity InventoryEvent for table inventory_event.

---
+++ src/main/java/vidar/websystem/domain/InventoryEvent.java
@@ -0,0 +1,38 @@
+package vidar.websystem.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author yishi.xing
+ * create datetime 4/10/2024 10:20 AM
+ * description Record of inventory add/update event
+ */
+@Data
+@Entity
+@Table(name = "inventory_event")
+public class InventoryEvent {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "inventory_id", nullable = false)
+    private Long inventoryId;
+
+    @Column(name = "inventory_event_type_id", nullable = false)
+    private Long inventoryEventTypeId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_time", updatable = false)
+    private Date createTime;
+
+    @Column(name = "create_user_id", updatable = false)
+    private Long createUserId;
+
+    @Column(name = "quantity", precision = 5, scale = 1, nullable = false)
+    private BigDecimal quantity;
+}
