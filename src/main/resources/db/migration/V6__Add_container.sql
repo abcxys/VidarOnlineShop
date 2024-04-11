c8789f0b4fc94122b24387a24a64095cd098ab2e
abcxys <abcxys92@gmail.com>
2024-04-11 14:27:50 -0400

    Add new entries into table 'container_status'.
    Use 5 different status to describe full lifecycle of a container.

---
+++ src/main/resources/db/migration/V6__Add_container.sql
@@ -0,0 +1,5 @@
+INSERT INTO container_status (id, status_alias, status_name, description) VALUES (1, 'FAC', 'Factory create', null);
+INSERT INTO container_status (id, status_alias, status_name, description) VALUES (2, 'OW', 'On water', null);
+INSERT INTO container_status (id, status_alias, status_name, description) VALUES (3, 'POT', 'At port', null);
+INSERT INTO container_status (id, status_alias, status_name, description) VALUES (4, 'OR', 'On rail', null);
+INSERT INTO container_status (id, status_alias, status_name, description) VALUES (5, 'ARR', 'Arrive', null);
