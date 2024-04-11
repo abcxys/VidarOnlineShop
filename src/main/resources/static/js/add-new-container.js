8067a5d2731dda6f9b4ab44a8e6f3a840ca2ccc8
abcxys <abcxys92@gmail.com>
2024-04-11 12:51:11 -0400

    Add web page design and javascript functions to render add-new-container page.
    Note: it's my first time to render selectpicker dynamically with the help of RESTful API.

---
+++ src/main/resources/static/js/add-new-container.js
@@ -0,0 +1,54 @@
+function populateSelect(node) {
+    $.ajax({
+        url: '/productApi/getProductDict',
+        method: 'GET',
+        success: function(response) {
+            // Clear existing options
+            node.empty();
+            node.append($('<option>', {
+                value: "",
+                text: "Select product"
+            }))
+            // Populate select with received options
+            response.forEach(function(option) {
+                node.append($('<option>', {
+                    value: option.id,
+                    text: option.width +'\" ' + option.woodSpeciesName.split(" ")[option.woodSpeciesName.split(" ").length - 1]
+                        + " " + option.colorName
+                }));
+            });
+
+            // Refresh the selectpicker to reflect changes
+            node.selectpicker('refresh');
+            node.hide();
+        },
+        error: function(xhr, status, error) {
+            console.error(error);
+        }
+    });
+}
+
+$('#addProductBtn').on('click', function() {
+    // Append a new row to the product quantities table
+    $('#productQuantities tbody').append(`
+        <tr>
+            <td>
+                <select class="selectpicker form-control productSelector" data-live-search="true">
+                </select>
+            </td>
+            <td>
+                <input type="number" class="form-control" name="quantity" required>
+            </td>
+            <td>
+                <input type="number" class="form-control" name="box" required>
+            </td>
+        </tr>
+    `);
+    populateSelect($('.productSelector').last());
+    //$(".productSelector").last().append($('#mySelect').clone().children());
+    //$('.productSelector').last().val("")
+    //$(".productSelector").last().selectpicker('refresh');
+});
+$(document).ready(function(){
+    //populateSelect($('#mySelect'));
+});
