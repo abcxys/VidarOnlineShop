4e1b34c22c49118bb37668f7d9d5c4416bf60d79
abcxys <abcxys92@gmail.com>
2024-04-13 10:19:41 -0400

    Add javascript function to handle containerForm submission event.
    Pass json data to server-side via ajax.

--- src/main/resources/static/js/add-new-container.js
+++ src/main/resources/static/js/add-new-container.js
@@ -30,7 +30,7 @@ function populateSelect(node) {
 
 $('#addProductBtn').on('click', function() {
     // Append a new row to the product quantities table
-    $('#productQuantities tbody').append(`
+    $('#productQuantitiesForm tbody').append(`
         <tr>
             <td>
                 <select class="selectpicker form-control productSelector" data-live-search="true">
@@ -46,6 +46,59 @@ $('#addProductBtn').on('click', function() {
     `);
     populateSelect($('.productSelector').last());
 });
+
+function serializeTableData() {
+    let tableData = [];
+    $('#productQuantitiesForm tbody tr').each(function(){
+        let rowData = {
+            productId: $(this).find('td:eq(0) select').val(),
+            skid: $(this).find('td:eq(1) input').val(),
+            box: $(this).find('td:eq(2) input').val()
+        };
+        tableData.push(rowData);
+    });
+    console.log(tableData);
+    $('#containerItems').val(JSON.stringify(tableData));
+}
+
+$('form#containerForm').submit(function(event){
+    event.preventDefault();
+
+    let tableData = [];
+    $('#productQuantitiesForm tbody tr').each(function(){
+        let rowData = {
+            productId: $(this).find('td:eq(0) select').val(),
+            skid: $(this).find('td:eq(1) input').val(),
+            box: $(this).find('td:eq(2) input').val()
+        };
+        tableData.push(rowData);
+    });
+
+    let jsonData = {
+        "containerNumber": $('#containerNumber').val(),
+        "billOfLandingNumber": $('#billOfLandingNumber').val(),
+        "shippingCompany": $('#shippingCompany').val(),
+        "freightForwarder": $('#freightForwarder').val(),
+        "portName": $('#portName').val(),
+        "containerItems": tableData,
+        "eta": $('#eta').datepicker('getDate')
+    };
+
+    $.ajax({
+        url: $(this).attr('action'),
+        type: $(this).attr('method'),
+        data: JSON.stringify(jsonData),
+        contentType: "application/json",
+        success: function(response){
+            console.log('success');
+            bootboxAlertPrompt('Container successfully added');
+        },
+        error: function(xhr, status, error){
+
+        }
+    });
+});
+
 $(document).ready(function(){
     //populateSelect($('#mySelect'));
     $('.datepicker').datepicker({
