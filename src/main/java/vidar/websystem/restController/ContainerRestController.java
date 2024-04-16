ee691973ca3cffc13ada558fd32c525392345945
abcxys <abcxys92@gmail.com>
2024-04-15 22:53:18 -0400

    Add REST api for update container info and retrieve container product info.

--- src/main/java/vidar/websystem/restController/ContainerRestController.java
+++ src/main/java/vidar/websystem/restController/ContainerRestController.java
@@ -2,17 +2,25 @@ package vidar.websystem.restController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import net.sf.json.JSONObject;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vidar.websystem.constants.PathConstants;
 import vidar.websystem.domain.Container;
 import vidar.websystem.domain.DatatablesView;
+import vidar.websystem.domain.ProductContainerItem;
+import vidar.websystem.domain.User;
 import vidar.websystem.dto.request.SearchRequest;
 import vidar.websystem.service.InventoryService;
+import vidar.websystem.service.UserService;
 
 /**
  * @author yishi.xing
@@ -24,12 +32,40 @@ import vidar.websystem.service.InventoryService;
 @RequiredArgsConstructor
 @RequestMapping(PathConstants.CONTAINER)
 public class ContainerRestController {
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
     private final InventoryService inventoryService;
+    private final UserService userService;
 
     @PostMapping(value = "/getFilteredContainers")
     public String getFilteredContainers(SearchRequest searchRequest, Model model, Pageable pageable){
         log.info("returning filtered containers' info");
         DatatablesView<Container> datatablesView = inventoryService.getFilteredContainers();
         return JSONObject.fromObject(datatablesView).toString();
+    }
+
+    @PostMapping(value = "/getItemsByContainerId")
+    public String getItemsByContainerId(@RequestParam("containerId") Long containerId,
+                                        Model model, Pageable pageable){
+        DatatablesView<ProductContainerItem> productContainerItem = inventoryService.getContainerItemsById(containerId);
+        return JSONObject.fromObject(productContainerItem).toString();
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<?> updateContainer(@RequestParam("id") Long id,
+                                             @RequestParam("containerNumber") String containerNumber,
+                                             @RequestParam("billOfLandingNumber") String billOfLandingNumber,
+                                             @RequestParam("estimatedArrivalDate") String estimatedArrivalDate,
+                                             @RequestParam("containerStatusId") Long containerStatusId){
+        User user = userService.getAuthenticatedUser();
+        Container container = inventoryService.getContainerById(id);
+        container.setContainerNumber(containerNumber);
+        container.setBillOfLandingNumber(billOfLandingNumber);
+        try {
+            container.setEstimatedArrivalDate(formatter.parse(estimatedArrivalDate));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        container.setContainerStatusId(containerStatusId);
+        return ResponseEntity.ok(inventoryService.updateContainer(user, container));
     }
 }
