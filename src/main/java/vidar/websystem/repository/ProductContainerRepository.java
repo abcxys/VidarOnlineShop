951c4f3ffc13912c329c71c5863172a51a4840ad
abcxys <abcxys92@gmail.com>
2024-04-15 23:02:54 -0400

    Add native ProductContainerRepository query for retrieving container product info.

--- src/main/java/vidar/websystem/repository/ProductContainerRepository.java
+++ src/main/java/vidar/websystem/repository/ProductContainerRepository.java
@@ -1,7 +1,10 @@
 package vidar.websystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import vidar.websystem.domain.ProductContainerItem;
+
+import java.util.List;
 
 /**
  * @author yishi.xing
@@ -9,4 +12,6 @@ import vidar.websystem.domain.ProductContainerItem;
  * description
  */
 public interface ProductContainerRepository extends JpaRepository<ProductContainerItem, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM container_floors cf WHERE cf.container_id = :containerId")
+    List<ProductContainerItem> findByContainerId(Long containerId);
 }
