324e77b5dd4d859d356107071d563b17abebfafd
abcxys <abcxys92@gmail.com>
2024-04-20 12:49:44 -0400

    Add sales_rep entity/repository classes.

---
+++ src/main/java/vidar/websystem/domain/SalesRep.java
@@ -0,0 +1,37 @@
+package vidar.websystem.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * @author yishi.xing
+ * create datetime 4/19/2024 4:52 PM
+ * description
+ */
+@Entity
+@Table(name = "sales_reps")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class SalesRep {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "alias")
+    private String alias;
+
+    @Column(name = "description")
+    private String description;
+}
---
+++ src/main/java/vidar/websystem/repository/SalesRepRepository.java
@@ -0,0 +1,12 @@
+package vidar.websystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vidar.websystem.domain.SalesRep;
+
+/**
+ * @author yishi.xing
+ * create datetime 4/20/2024 11:37 AM
+ * description
+ */
+public interface SalesRepRepository extends JpaRepository<SalesRep, Long> {
+}
