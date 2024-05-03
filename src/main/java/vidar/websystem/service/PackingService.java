4b02b9e8e34a0c1564c4c9cdf06933f481cd55e4
abcxys <abcxys92@gmail.com>
2024-05-03 15:24:44 -0400

    Add addPackingSlip service/impl methods.
    Note that only packing slip is created, the associated 'sales_order_packing' entries are not added.

--- src/main/java/vidar/websystem/service/PackingService.java
+++ src/main/java/vidar/websystem/service/PackingService.java
@@ -1,8 +1,11 @@
 package vidar.websystem.service;
 
+import org.springframework.http.ResponseEntity;
 import vidar.websystem.domain.DatatablesView;
 import vidar.websystem.domain.Driver;
 import vidar.websystem.domain.SalesOrder;
+import vidar.websystem.domain.User;
+import vidar.websystem.dto.request.PackingSlipRequest;
 
 import java.util.List;
 
@@ -14,4 +17,6 @@ public interface PackingService {
 	DatatablesView<SalesOrder> getAllOrders();
 
 	List<Driver> getDrivers();
+
+	ResponseEntity<?> addPackingSlip(User user, PackingSlipRequest packingSlipRequest);
 }
--- src/main/java/vidar/websystem/service/impl/PackingServiceImpl.java
+++ src/main/java/vidar/websystem/service/impl/PackingServiceImpl.java
@@ -1,14 +1,21 @@
 package vidar.websystem.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
-import vidar.websystem.domain.DatatablesView;
-import vidar.websystem.domain.Driver;
-import vidar.websystem.domain.SalesOrder;
+import org.springframework.transaction.annotation.Transactional;
+import vidar.websystem.constants.ErrorMessage;
+import vidar.websystem.constants.SuccessMessage;
+import vidar.websystem.domain.*;
+import vidar.websystem.dto.request.PackingSlipRequest;
 import vidar.websystem.repository.DriverRepository;
+import vidar.websystem.repository.PackingSlipRepository;
+import vidar.websystem.repository.PackingStatusRepository;
 import vidar.websystem.repository.SalesOrderRepository;
 import vidar.websystem.service.PackingService;
 
@@ -16,12 +23,14 @@ import vidar.websystem.service.PackingService;
  * @author yishi.xing
  * @created Feb 15, 2024 - 10:50:47 PM
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PackingServiceImpl implements PackingService{
-	
-	private final SalesOrderRepository salesOrderRepository;
 
+	private final PackingSlipRepository packingSlipRepository;
+	private final SalesOrderRepository salesOrderRepository;
+	private final PackingStatusRepository packingStatusRepository;
 	private final DriverRepository driverRepository;
 
 	@Override
@@ -41,6 +50,30 @@ public class PackingServiceImpl implements PackingService{
 	@Override
 	public List<Driver> getDrivers() {
 		return driverRepository.findAll();
+	}
+
+	/**
+	 * @param user Authenticated user.
+	 * @param packingSlipRequest packingSlipRequest contains driver info and packingSlipItems.
+	 * @return ResponseEntity
+	 */
+	@Transactional
+	@Override
+	public ResponseEntity<?> addPackingSlip(User user, PackingSlipRequest packingSlipRequest) {
+		// Create packing slip with status 'created'
+		PackingSlip packingSlip = new PackingSlip();
+		packingSlip.setPackingStatus(packingStatusRepository.findById(1L));
+		packingSlip.setDriver(driverRepository.findById(packingSlipRequest.getDriverId()).orElse(null));
+		packingSlip.setCreateUserId(user.getId());
+		packingSlip.setCreateTime(new Date());
+		try {
+			packingSlipRepository.save(packingSlip);
+			packingSlipRepository.flush();
+			log.info("The inserted packing slip returns id = " + packingSlip.getId());
+		} catch (Exception e){
+			return ResponseEntity.badRequest().body(ErrorMessage.PACKING_SLIP_CREATED_FAILED);
+		}
+		return ResponseEntity.ok().body(SuccessMessage.PACKING_SLIP_CREATED);
 	}
 
 }
