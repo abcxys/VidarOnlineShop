831d74fa62b28c259666bfeb4e8b00c21eec1a13
abcxys <abcxys92@gmail.com>
2024-05-26 23:45:49 -0400

    Update one of the user info to order@vidarflooring.com

--- src/main/resources/db/migration/V2__Add_users.sql
+++ src/main/resources/db/migration/V2__Add_users.sql
@@ -8,7 +8,7 @@ values(2, 'test123@test.com', 'John', 'Doe', 'New York', 'Wall Street1', '123456
 
 -- password: admin
 insert into users(id, email, first_name, last_name, city, address, phone_number, post_index, activation_code, active, password, password_reset_code)
-values(3, 'ivan123@test.com', 'Ivan', 'Ivanov', 'Moscow', 'Tverskaya street 1', '1234567890', '1234567890', null, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', null);
+values(3, 'order@vidarflooring.com', 'Samantha', 'Gao', 'Mississauga', '1775 Sismet Road', '1234567890', '1234567890', null, true, '$2a$08$eApn9x3qPiwp6cBVRYaDXed3J/usFEkcZbuc3FDa74bKOpUzHR.S.', null);
 
 insert into user_role (user_id, roles) values (1, 'ADMIN');
 insert into user_role (user_id, roles) values (2, 'USER');
