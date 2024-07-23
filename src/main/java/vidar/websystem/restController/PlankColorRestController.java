9722ad9b989e50e49a2078aa9c35dc2d485f5737
abcxys <abcxys92@gmail.com>
2024-07-22 23:53:29 -0400

    Add REST controller for PlankColor.

---
+++ src/main/java/vidar/websystem/restController/PlankColorRestController.java
@@ -0,0 +1,19 @@
+package vidar.websystem.restController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yishi.xing
+ * create datetime 7/22/2024 11:52 PM
+ * description
+ */
+
+@RestController
+@RequestMapping("/plankColor")
+@RequiredArgsConstructor
+@Slf4j
+public class PlankColorRestController {
+}
