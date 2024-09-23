8a330ba9554b93d238b75d46a877c0c10ee6b3e8
abcxys <abcxys92@gmail.com>
2024-09-22 22:51:32 -0400

    Add a util class to generate json token and extract username out of token.

---
+++ src/main/java/vidar/websystem/utils/JwtUtil.java
@@ -0,0 +1,36 @@
+package vidar.websystem.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author yishi.xing
+ * create datetime 9/22/2024 10:43 PM
+ * description
+ */
+@Component
+public class JwtUtil {
+
+    private static final String SECRET = "your-secret-key";
+
+    private static final long EXPIRATION_TIME = 864_000_000;
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.ES512, SECRET)
+                .compact();
+    }
+
+    public static String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
